8010b21278e6e2d509119b2279f4e8cd59482145
lior.banai <liorbanai@gmail.com>
2022-08-20 11:59:33 +0300

    restore old vbrpoj file

---
+++ SPE 2/My Project/AssemblyInfo.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Resources
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Stiffened Plates Evaluator")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Lior Banai")>
+<Assembly: AssemblyProduct("Stiffened Plates Evaluator")>
+<Assembly: AssemblyCopyright("Lior Banai © 2005-2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1f66b928-a0c6-4554-bd6f-d094b3e67453")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+
+<Assembly: AssemblyVersion("2.1.0.0")>
+<Assembly: AssemblyFileVersion("2.1.0.0")>
+<Assembly: NeutralResourcesLanguage("en-US")>
